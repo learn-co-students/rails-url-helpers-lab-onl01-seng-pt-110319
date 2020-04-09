56f61343b1fc0847c93bc9b6c54a66bc9d50d545
Wills Blake <richwblake@gmail.com>
2020-04-08 20:05:11 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create!(first_name: "Wills", last_name: "Blake")
+Student.create!(first_name: "Kaelah", last_name: "Scheff")
+Student.create!(first_name: "Joshua", last_name: "Hollingsworth")
+Student.create!(first_name: "Billy", last_name: "Childers")
+Student.create!(first_name: "Peyton", last_name: "Hooker")
